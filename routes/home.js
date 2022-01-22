09971e9727ba67c39a8147c1642584f47f3cc3aa
mauriciochavezpe <mauchp0311@gmail.com>
2022-01-22 18:46:16 -0500

    prueba

--- routes/home.js
+++ routes/home.js
@@ -10,6 +10,7 @@ router.get('/', function(req, res, next) {
     res.redirect('/history');
   }
   */
+ console.log("prueba");
   const data = {
     title: 'Inicio - DoctCom Money Exchange',
     user: {
