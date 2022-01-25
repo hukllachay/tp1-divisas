cdfe5735d5a66151de5c4446606d3d3fd790b922
anthony onori <anthony.onori@hotmail.com>
2022-01-24 23:14:44 -0500

    Anthony - Cambios modulo operacion

---
+++ src/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: '1234',
+        database: 'tp_divisa',
+        insecureAuth : true
+    },
+    database1: {
+        host: 'fdb33.awardspace.net',
+        user: '3939607_upctf',
+        password: '-ZZVEr^47J9U2#S.',
+        database: '3939607_upctf'
+    }
+}
--- src/routes/operation.js
+++ src/routes/operation.js
@@ -1,13 +1,28 @@
 var express = require('express');
+var soap = require('soap');
 var router = express.Router();
+
+var pool = require('../database');
 
 /* GET home page. */
 
-router.get('/', function(req, res, next) {
-  const data = {
-    title:  'Operación - DotCom Money Exchange',
-  }
-  res.render('operation', { data: data });
+router.get('/', async function(req, res, next) {
+  var ratio = 0;
+  var bancos = await pool.query('CALL usp_bancolistar()');
+  console.log(bancos[0]);
+  // Consumo del servicio desde el cliente
+  var url = 'http://192.190.42.160/ServicioDivisa/CambioActual.asmx?wsdl';
+  soap.createClientAsync(url).then((client) => {
+    return client.DivisaActual({}, function(arg, arg2) {
+      ratio = arg2.DivisaActualResult;
+      const data = {
+        title: 'Iniciar operación de cambio de divisas', //'Operación - DotCom Money Exchange',
+        ratio: ratio,
+        bancos: bancos[0]
+      }
+      res.render('operation', { data: data });
+    });
+  });
 });
 
 router.get('/:step', function(req, res, next) {
