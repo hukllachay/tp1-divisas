0db03580775d3f6e5265c48540006cdd7e89bc1d
anthony onori <anthony.onori@hotmail.com>
2022-02-14 22:32:07 -0500

    Anthony - Agregar session

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var soap = require('soap');
 var router = express.Router();
+var LocalStorage = require('node-localstorage').LocalStorage;
+var localStorage = new LocalStorage('./scratch');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
   var ratio = 0;
+  var session = JSON.parse(localStorage.getItem('userSession'));
   // Consumo del servicio desde el cliente
   var url = 'http://192.190.42.160/ServicioDivisa/CambioActual.asmx?wsdl';
   soap.createClientAsync(url).then((client) => {
     return client.DivisaActual({}, function(arg, arg2){
       ratio = arg2.DivisaActualResult;
-      const data = {
+      var data = {
         title:  'Inicio - DotCom Money Exchange',
         ratio: ratio,
-        modoCambio: 'SD'
+        modoCambio: 'SD',
+        userInfo: session
       }
       res.render('index', { data: data });
     });
